b5cc37f087e5420f77b6039bc8381379b747d6a2
MauricioMontoya1 <mauricio.montoya@momentumdigital.com.co>
2023-05-17 11:57:06 -0500

    Feature (Footer): Se agrega sección footer, con todos los recursos multimedia y datos

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import { HistorySection } from '@/containers/home/HistorySection'
 import { PropertySection } from '@/containers/home/PropertySection'
 import { ServiceSection } from '@/containers/home/ServiceSection'
 import { TestimonialSection } from '@/containers/home/TestimonialSection'
+import { Footer } from '../components/Footer'
 
 export const metadata = {
     title: 'Comprar casa en Colombia desde el exterior | Colraices',
@@ -17,6 +18,7 @@ export default function HomePage() {
             <ServiceSection />
             <HistorySection />
             <TestimonialSection />
+            <Footer></Footer>
         </>
     )
 }
