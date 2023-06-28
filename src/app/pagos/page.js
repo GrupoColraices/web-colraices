b699bbdbd695d0294945588941d74668061197f8
cristiancolraices <cristian.orrego@colraices.co>
2023-06-28 10:30:33 -0500

    fix (pagos/page):Ajusta los meta tags

--- src/app/pagos/page.js
+++ src/app/pagos/page.js
@@ -3,11 +3,6 @@ import { BannerSection } from '@/containers/pagos/BannerSection'
 import { Checkout } from '@/containers/pagos/Checkout'
 import useCart from '@/hooks/useCart'
 
-export const metadata = {
-    title: 'Comprar casa en Colombia desde el exterior | Colraices',
-    description: 'Comprar casa en Colombia desde el exterior | Colraices',
-}
-
 export default function PagosPage() {
     const { cart, addToCart, removeFromCart, addQuantity, subQuantity, totalAmount } = useCart()
     const handleAddToCard = (service) => {
