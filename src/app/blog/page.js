300e74459485d33a696bf48c40ca59fdd4cde9e4
daniel2710 <daniel2710es@gmail.com>
2023-12-03 15:36:28 -0500

    feature(partners section - blog)

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,9 +1,14 @@
+import { Partners } from "@/components/Partners";
 import BannerSectionBlog from "@/containers/home/blog/BannerSectionBlog";
+import { partners } from "@/helpers";
 
 export default function BlogPage() {
     return(
         <main>
             <BannerSectionBlog/>
+            <div className='partners'>
+                <Partners partners={partners} hover={true} />
+            </div>
         </main>
     )
 }
