c92a0cfe579647f7e6741da95e5992fec1bde7a3
daniel2710 <daniel2710es@gmail.com>
2023-12-05 22:16:03 -0500

    feature(articles section layout - blog)

--- src/containers/blog/ArticlesSection.jsx
+++ src/containers/blog/ArticlesSection.jsx
@@ -2,6 +2,7 @@
 import Line from '@/components/Line';
 import CardArticleLarge from '@/components/blog/CardArticleLarge';
 import CardArticleMedium from '@/components/blog/CardArticleMedium';
+import CardArticleSm from '@/components/blog/CardArticleSm';
 import '@/sass/containers/blog/ArticlesSection.scss';
 
 const ArticlesSection = () => {
@@ -17,6 +18,12 @@ const ArticlesSection = () => {
         <div className='container-cards-medium'>
           <CardArticleMedium />
           <CardArticleMedium />
+        </div>
+
+        <div className='container-cards-sm-desktop'>
+          <CardArticleSm />
+          <CardArticleSm />
+          <CardArticleSm />
         </div>
       </div>
     </section>
