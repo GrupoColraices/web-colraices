0bd76a219976a69a5a551a32a3ee7a220f246652
daniel2710 <daniel2710es@gmail.com>
2023-12-05 23:49:41 -0500

    feature(podcast layout component - blog)

--- src/components/blog/Podcast.jsx
+++ src/components/blog/Podcast.jsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Line from "../Line"
 import '@/sass/components/blog/Podcast.scss';
 import podcast from '../../../public/podcast.svg';
+import podcastDesktop from '../../../public/podcast-desktop.svg';
 
 const Podcast = () => {
   return (
@@ -11,7 +12,8 @@ const Podcast = () => {
         <Line width='20' />
 
         <div className="podcast-content">
-            <Image src={podcast} alt="podcast" width={200} height={200} />
+          <Image className="podcast-image" src={podcast} alt="podcast" width={200} height={200} />
+          <Image className="podcast-image-desktop" src={podcastDesktop} alt="podcast" width={200} height={200} />
         </div>
     </div>
   )
