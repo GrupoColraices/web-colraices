cfa58c9e7c994994ccccdcd95cbd6a1a47d1d0bd
daniel2710 <daniel2710es@gmail.com>
2023-12-09 13:27:45 -0500

    feature(add google font from next js)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,17 @@
 import { Header } from '@/components/Header'
 import '../sass/main.scss'
 import { Footer } from '@/components/Footer'
+import { Montserrat } from 'next/font/google';
+
+const montserrat = Montserrat({
+    subsets: ['latin'],
+    weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
+})
 
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body>
+            <body className={montserrat.className}>
                 <Header />
                 {children}
                 <Footer />
