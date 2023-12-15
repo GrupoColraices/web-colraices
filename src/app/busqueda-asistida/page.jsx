dc6cf864d5f3f1c928702c2187501ee99b330c17
MauricioMontoya1 <mmontoyaabadia@gmail.com>
2023-12-15 06:50:37 -0500

    Feature (page - busqueda asistida): Se agrega pagina con formulario

---
+++ src/app/busqueda-asistida/page.jsx
@@ -0,0 +1,10 @@
+import { ItemFormResultado } from "@/components/ItemFormResultado"
+import '@/sass/containers/busqueda-asistida/AssistedSearch.scss'
+export default function AssistedSearch() {
+    return (
+        <section className="assisted-search">
+            <ItemFormResultado />
+        </section>
+
+    )
+}
