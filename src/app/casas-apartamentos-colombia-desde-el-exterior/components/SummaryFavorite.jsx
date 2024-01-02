2136950450db7bb53615e2c6cd2f772b9c880e3d
Mmontoya1 <mauricio.montoya@colraices.co>
2024-01-02 08:24:34 -0500

    Feature (SummaryFavorite): Se restructura componente

--- src/app/casas-apartamentos-colombia-desde-el-exterior/components/SummaryFavorite.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/components/SummaryFavorite.jsx
@@ -2,21 +2,29 @@ import React from 'react'
 
 export const SummaryFavorite = ({ icon, title, content }) => {
     return (
-        <details>
+        <details open>
             <summary>
-                <span class="summary-title">{icon}{title}</span>
+                <span class="summary-title"><img src={icon} alt="Icon" />{title}</span>
                 <div class="summary-chevron-up">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down"><polyline points="6 9 12 15 18 9"></polyline></svg>
                 </div>
             </summary>
 
             <div class="summary-content">
                 <p>
-                    {content}
+                    {content[0]}
+                </p>
+                <div className='vertical-line'></div>
+                <p>
+                    {content[1]}
+                </p>
+                <div className='vertical-line'></div>
+                <p>
+                    {content[2]}
                 </p>
             </div>
             <div class="summary-chevron-down">
-                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
             </div>
         </details>
     )
