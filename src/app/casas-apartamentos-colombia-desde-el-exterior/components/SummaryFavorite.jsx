126cc31b2b5dc905485a5ae8b24e531770ee1045
MauricioMontoya1 <mmontoyaabadia@gmail.com>
2024-01-04 00:12:39 -0500

    Refactor (SideMenuFavorites): Se refactoriza el codigo al iterar con cada elemento

--- src/app/casas-apartamentos-colombia-desde-el-exterior/components/SummaryFavorite.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/components/SummaryFavorite.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useCurrency } from '../hooks/useCurrency';
 
 export const SummaryFavorite = ({ icon, title, content }) => {
+  const [formatePrice] = useCurrency();
+
     return (
         <details open>
             <summary>
@@ -11,17 +14,14 @@ export const SummaryFavorite = ({ icon, title, content }) => {
             </summary>
 
             <div class="summary-content">
-                <p>
-                    {content[0]}
-                </p>
-                <div className='vertical-line'></div>
-                <p>
-                    {content[1]}
-                </p>
-                <div className='vertical-line'></div>
-                <p>
-                    {content[2]}
-                </p>
+                {content?.map((item, index) => (
+                    <React.Fragment key={item.id}>
+                        {index > 0 && <div className='vertical-line'></div>}
+                        <p className='text-cities'>
+                            {formatePrice(item.precio)}
+                        </p>
+                    </React.Fragment>
+                ))}
             </div>
             <div class="summary-chevron-down">
                 <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-up"><polyline points="18 15 12 9 6 15"></polyline></svg>
