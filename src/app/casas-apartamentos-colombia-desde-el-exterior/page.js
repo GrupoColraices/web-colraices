eb55f1610c7a19c665afd4ff6939ad847f2c5224
MauricioMontoya1 <mmontoyaabadia@gmail.com>
2024-01-11 11:38:36 -0500

    Fix (page Portal-Inmobiliario): Se elimina el script de Hotjar Analytics

--- src/app/casas-apartamentos-colombia-desde-el-exterior/page.js
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/page.js
@@ -12,23 +12,14 @@ export default function Home() {
   return (
     <>
       <Script async src="https://www.googletagmanager.com/gtag/js?id=G-03VJLYKNTV" />
-        <Script id="google-analytics">
-            {` window.dataLayer = window.dataLayer || [];
+      <Script id="google-analytics">
+        {` window.dataLayer = window.dataLayer || [];
                 function gtag(){dataLayer.push(arguments);}
                 gtag('js', new Date());
                 gtag('config', 'G-03VJLYKNTV');`}
-        </Script>
+      </Script>
 
-        <Script id='HotJarAnalytics'
-            dangerouslySetInnerHTML={{
-                __html: `(function (h, o, t, j, a, r) {
-                    h.hj = h.hj || function () { (h.hj.q = h.hj.q || []).push(arguments) };
-                    h._hjSettings = { hjid: 3262680, hjsv: 6 };
-                    a = o.getElementsByTagName('head')[0];
-                    r = o.createElement('script'); r.async = 1;
-                    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
-                    a.appendChild(r);
-                })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');`}} />
+
       <Toaster containerStyle={{ zIndex: 10000000 }} position="top-right" />
       <Banner main />
       <FeaturedProperties />
