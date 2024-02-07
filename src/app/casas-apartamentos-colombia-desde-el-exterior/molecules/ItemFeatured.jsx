8de395ce99831c19f226b3981113498ef60819b8
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-06 21:53:04 -0500

    refactor (Swiper): se refactoriza componente

--- src/app/casas-apartamentos-colombia-desde-el-exterior/molecules/ItemFeatured.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/molecules/ItemFeatured.jsx
@@ -1,22 +1,56 @@
+'use client'
 import Link from 'next/link'
 import { MdLocationOn } from 'react-icons/md'
 import { useCurrency } from '../hooks/useCurrency'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
 
 export default function ItemFeatured({ itemProperty }) {
-    const { thumbnail, titulo, precio, ciudad, region, area_const, habitaciones, baños, slug } = itemProperty;
     const [formatePrice] = useCurrency();
     return (
-        <div className='container__item-featured'>
-            <article>
-                <div>
-                    <img src={thumbnail} alt="Image Property" />
-                </div>
-                <h3>{titulo}</h3>
-                <p className='price'>Desde {formatePrice(precio)} COP</p>
-                <p className='ubication'><MdLocationOn className="icon" />{ciudad} - {region}</p>
-                <ul className='information'><li>Desde {area_const}m²</li> <li>{habitaciones} Hab</li> <li>{baños} Baños</li></ul>
-                <button className='btn-view' type='button'><Link href={`/casas-apartamentos-colombia-desde-el-exterior/inmueble/${slug}`}>Ver Proyecto</Link></button>
-            </article>
-        </div>
+        <Swiper
+            className='swiper-item-featured'
+            navigation={true}
+            pagination={{ dynamicBullets: true }}
+            modules={[Autoplay, Pagination, Navigation]}
+            slidesPerView={1}
+            spaceBetween={8}
+            loop={true}
+            autoplay={{ delay: 3000, disableOnInteraction: false }}
+            breakpoints={{
+                640: {
+                    slidesPerView: 2,
+                    spaceBetween: 8,
+                },
+                930: {
+                    slidesPerView: 1,
+                    spaceBetween: 1,
+                },
+            }}
+        >
+            {
+                itemProperty?.map((item) => (
+                    <SwiperSlide key={item.id}>
+                        <div className='container-item-featured'>
+                            <article>
+                                <div>
+                                    <img src={item.thumbnail} alt="Image Property" />
+                                </div>
+                                <h3>{item.titulo}</h3>
+                                <p className='price'>Desde {formatePrice(item.precio)} COP</p>
+                                <p className='ubication'><MdLocationOn className="icon" />{item.ciudad} - {item.region}</p>
+                                <ul className='information'><li>Desde {item.area_const}m²</li> <li>{item.habitaciones} Hab</li> <li>{item.baños} Baños</li></ul>
+                                <button className='btn-view' type='button'><Link href={`/casas-apartamentos-colombia-desde-el-exterior/inmueble/${item.slug}`}>Ver Proyecto</Link></button>
+                            </article>
+                        </div>
+                    </SwiperSlide>
+                ))
+            }
+        </Swiper>
     )
 }
+
