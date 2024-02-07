b98915b16daa9c785f5dc4e0abb71ed297545cbc
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-06 22:00:04 -0500

    Feat (): se crea nuevo componente para los inmuebles recientes

---
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/molecules/ItemReciente.jsx
@@ -0,0 +1,44 @@
+'use client'
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Navigation } from "swiper";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import { ItemInmueble } from "../molecules/ItemInmueble";
+
+export const ItemReciente = ({ inmRecientes }) => {
+    return (
+
+        <div className="container__reciente">
+            < Swiper
+                navigation={true}
+                pagination={{ dynamicBullets: true }
+                }
+                modules={[Pagination, Navigation]}
+                slidesPerView={1}
+                spaceBetween={8}
+                loop={true}
+                breakpoints={{
+                    640: {
+                        slidesPerView: 2,
+                        spaceBetween: 2,
+                    },
+                    930: {
+                        slidesPerView: 3,
+                        spaceBetween: 5,
+                    },
+                }}
+            >
+                {
+                    inmRecientes?.map((inmuebleRecionete) => (
+                        <SwiperSlide key={inmuebleRecionete.id} className="mySlide">
+                            <ItemInmueble dataInmueble={inmuebleRecionete} />
+                        </SwiperSlide>
+                    ))
+                }
+
+            </Swiper >
+        </div >
+    )
+
+}
