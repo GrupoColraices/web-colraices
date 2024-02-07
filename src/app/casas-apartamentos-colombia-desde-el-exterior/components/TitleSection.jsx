d0fcda9d1b0996a8a837d18f43d99fbe3237772f
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-07 09:19:07 -0500

    refactor (etiqueta h2): se agrega etiqueta para mejorar accesibilidad de los elementos

--- src/app/casas-apartamentos-colombia-desde-el-exterior/components/TitleSection.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/components/TitleSection.jsx
@@ -1,7 +1,7 @@
 export const TitleSection = (props) => {
     return (
         <section className={`title-section ${props.shadow && 'shadow'}`} data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="111" data-aos-offset="0">
-            <h4 className='title-section-title'>{props.title} <span>{props.span}</span></h4>
+            <h2 className='title-section-title'>{props.title} <span>{props.span}</span></h2>
             <p className='title-section-paragraph'>{props.children}</p>
         </section>
     )
