f4c650d6397e8a4c18dc97616934786f2531a76a
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-07 09:18:35 -0500

    refactor ("use client"): se elimina del componente TitleNavbar la directiva use client

--- src/app/casas-apartamentos-colombia-desde-el-exterior/components/TitleNavbar.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/components/TitleNavbar.jsx
@@ -1,5 +1,3 @@
-'use client'
-
 export const TitleNavbar = (props) => {
     return (
         <section className={`title-section-navbar ${props.shadow && 'shadow'}`} data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="111" data-aos-offset="0">
