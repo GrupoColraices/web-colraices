99b3cc07066739e38145b4eaac3b958a0067390c
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-09 09:16:30 -0500

    chore (import TitleSection): se cambia la importación de componente

--- src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/Regiones.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/Regiones.jsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from "react";
 import { Link } from "next/link";
-import { TitleSection } from "../components/TitleSection";
+import TitleSection from "../components/TitleSection";
 import { ItemRegion } from "../molecules/ItemRegion";
 import { APIURL } from '../config';
 
