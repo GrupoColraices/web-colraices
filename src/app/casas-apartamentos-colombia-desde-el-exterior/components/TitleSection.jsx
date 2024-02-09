5445938d7f6c0bc3bc358216478753ffd204aeef
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-09 08:22:04 -0500

    refactor (block/million): se usa libreria million para optimizar renderizado de componentes

--- src/app/casas-apartamentos-colombia-desde-el-exterior/components/TitleSection.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/components/TitleSection.jsx
@@ -1,8 +1,18 @@
-export const TitleSection = (props) => {
+'use client'
+import { block } from 'million/react';
+import { useEffect, useState } from 'react';
+export default block(function TitleSection(props) {
+    const [isClient, setIsClient] = useState(false)
+
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
     return (
-        <section className={`title-section ${props.shadow && 'shadow'}`} data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="111" data-aos-offset="0">
-            <h2 className='title-section-title'>{props.title} <span>{props.span}</span></h2>
-            <p className='title-section-paragraph'>{props.children}</p>
-        </section>
+        isClient && (
+            <section className={`title-section ${props.shadow && 'shadow'}`} data-aos="fade-zoom-in" data-aos-easing="ease-in-back" data-aos-delay="111" data-aos-offset="0">
+                <h2 className='title-section-title'>{props.title} <span>{props.span}</span></h2>
+                <p className='title-section-paragraph'>{props.children}</p>
+            </section>
+        )
     )
-}
+})
