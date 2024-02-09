5629dcfcf5e2237b369feaa1f0ec45dd99786d33
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-09 09:15:58 -0500

    refactor (fetching): se elimina fetching de datos en el componente

--- src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/InmReciente.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/InmReciente.jsx
@@ -1,19 +1,13 @@
+import TitleSection from "../components/TitleSection";
+import ItemReciente from "../molecules/ItemReciente";
 
-import { TitleSection } from "../components/TitleSection";
-import { ItemReciente } from "../molecules/ItemReciente";
-import { APIURL } from '../config';
-
-
-export const InmReciente = async () => {
-    const fetching = await fetch(`${APIURL}properties`);
-    const response = await fetching.json();
-    const data = response?.data;
+export const InmReciente = ({ inmRecientes }) => {
     return (
         <section className="slider-recientes">
             <TitleSection title={"Nuevas"} span={'propiedades'}>
                 Los mejores proyectos en nuestro país los encuentras aquí, con diferentes tipos de inmuebles, en las mejores ciudades de Colombia.
             </TitleSection>
-            <ItemReciente inmRecientes={data} />
+            <ItemReciente inmRecientes={inmRecientes} />
         </section>
 
     );
