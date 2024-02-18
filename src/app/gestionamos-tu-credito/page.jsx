63bcc591c8287f70ea5005c1756487125469c285
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-18 17:41:10 -0500

    chore (ManagingYourCredit) : se cambia nombre de  archivo en la importación

--- src/app/gestionamos-tu-credito/page.jsx
+++ src/app/gestionamos-tu-credito/page.jsx
@@ -4,7 +4,7 @@ import { BankLoan } from '@/containers/gestionamos-tu-credito/BankLoan';
 import { Banner } from '@/containers/gestionamos-tu-credito/Banner';
 import { CreditRequest } from '@/containers/gestionamos-tu-credito/CreditRequest';
 import { FinancingLines } from '@/containers/gestionamos-tu-credito/FinancingLines';
-import '@/sass/containers/gestionamos-tu-credito/Managing-your-credit.scss'
+import '@/sass/containers/gestionamos-tu-credito/ManagingYourCredit.scss'
 
 export default function ManagingYourCredit() {
     return (
