719a522a4ec509a5ec25f392a77c2a05d1f4ba6e
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-18 17:27:19 -0500

    Feat (ManagingYourCredit): se agrega una nueva página

---
+++ src/app/gestionamos-tu-credito/page.jsx
@@ -0,0 +1,20 @@
+import { Header } from '@/components/Header';
+import { Form } from '@/components/gestionamos-tu-credito/Form';
+import { BankLoan } from '@/containers/gestionamos-tu-credito/BankLoan';
+import { Banner } from '@/containers/gestionamos-tu-credito/Banner';
+import { CreditRequest } from '@/containers/gestionamos-tu-credito/CreditRequest';
+import { FinancingLines } from '@/containers/gestionamos-tu-credito/FinancingLines';
+import '@/sass/containers/gestionamos-tu-credito/Managing-your-credit.scss'
+
+export default function ManagingYourCredit() {
+    return (
+        <>
+            <Header />
+            <Banner />
+            <BankLoan />
+            <CreditRequest />
+            <FinancingLines />
+            <Form />
+        </>
+    )
+}
