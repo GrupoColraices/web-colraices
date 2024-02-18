b167e6fd8a7833aa826fc6269616623f67bcc312
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-18 17:29:17 -0500

    Feat (BankLoan): se agrega un nuevo componente para la página gestionamos tu crédito

---
+++ src/containers/gestionamos-tu-credito/BankLoan.jsx
@@ -0,0 +1,27 @@
+import { Button } from "@/components/gestionamos-tu-credito/Button";
+import { Card } from "@/components/gestionamos-tu-credito/Card"
+import { TitleSection } from "../../components/gestionamos-tu-credito/TitleSection";
+import { Partners } from "@/components/Partners";
+import { partners } from "@/helpers";
+import '@/sass/containers/gestionamos-tu-credito/BankLoan.scss';
+export const BankLoan = () => {
+    return (
+        <section className="container-bank-loan">
+            <div className="content-bank-loan">
+                <div className="content-cards">
+                    <Card text={"GRATIS"} icon={"cash-in-hand"} position={"left"} />
+                    <Card text={"SIN ESPERAS"} icon={"clock"} position={"center"} color={"primary"} />
+                    <Card text={"SIN PAPELEOS"} icon={"paperwork"} position={"right"} />
+                </div>
+                <div className="content-text">
+                    <h2>¿Cuánto me prestan los<span> bancos en Colombia?</span></h2>
+                    <p>Te damos el resultado en <span>2 minutos </span>
+                        con Cupo de Crédito</p>
+                    <Button text={"Ver  mi cupo"} URL={"/"} />
+                </div>
+            </div>
+            <TitleSection text={"Te decimos cuánto puedes obtener, qué necesitas y"} span={"el valor de tu cuota con nuestros bancos aliados"} />
+            <Partners partners={partners} hover={true} customClass={"partners-managing-your-credit"} />
+        </section>
+    )
+}
