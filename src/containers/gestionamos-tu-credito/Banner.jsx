b0574d19eadce33b6d54f62462690f17995a5b3d
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-18 17:28:34 -0500

    Feat (Banner): se agrega un nuevo componente para la página gestionamos tu crédito

---
+++ src/containers/gestionamos-tu-credito/Banner.jsx
@@ -0,0 +1,21 @@
+import { Button } from '@/components/gestionamos-tu-credito/Button'
+import '@/sass/containers/gestionamos-tu-credito/Banner.scss';
+
+export const Banner = () => {
+    return (
+        <section className="container-manage-credit">
+            <div className="content-text">
+                <img src="/gestionamos-tu-credito/img/banner-icons.svg" alt="Image" />
+                <h1>Abrimos la puerta al crédito
+                    para tu casa en Colombia
+                </h1>
+                <p>En tiempo real, con resultados precisos
+                    y sin
+                    importar donde estés.
+                </p>
+                <Button text={"VER MAS"} URL={"/"} />
+            </div>
+            <img className='image-banner' src="/gestionamos-tu-credito/img/image-banner.webp" alt="Image" />
+        </section>
+    )
+}
