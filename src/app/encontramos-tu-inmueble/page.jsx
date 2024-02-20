461a43d7109ed0806e8a0107b8cdfe4ba5d0708d
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-20 18:06:43 -0500

    Feat (WeFoundYourProperty): se agrega una nueva página

---
+++ src/app/encontramos-tu-inmueble/page.jsx
@@ -0,0 +1,9 @@
+import { Banner } from '@/containers/encontramos-tu-inmueble/Banner'
+
+export default function WeFoundYourProperty() {
+    return (
+        <>
+            <Banner />
+        </>
+    )
+}
