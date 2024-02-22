4d103c51d754328e5792af09b6a7cfd931809490
cristianorregodev <cristian.duquew@gmail.com>
2024-02-22 16:47:40 -0500

    Fix (typo): Corrige ortografia del texto

--- src/containers/financia-tu-casa/FinancingBenefics.jsx
+++ src/containers/financia-tu-casa/FinancingBenefics.jsx
@@ -7,7 +7,7 @@ export const FinancingBenefics = () => {
                 <h2>Beneficios</h2>
                 <ul>
                     <li>Solicita y aprueba tu crédito desde el exterior. Firma sin necesidad de viajar a Colombia</li>
-                    <li>Financia hasta 80% de tu casa nueva o usada, o 70% para otro inmuebles.</li>
+                    <li>Financia hasta 80% de tu casa nueva o usada, o 70% para otro inmueble.</li>
                     <li>Elige cuota fija en pesos y aprovecha la tasa de cambio favorable</li>
                     <li>Escoge hasta 30 años para pagar tu crédito</li>
                     <li>Reduce el tiempo o valor de la cuota haciendo abonos o prepagos</li>
