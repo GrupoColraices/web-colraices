cd395696e13eb5b400af582ef568c6397d3fcdb8
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-22 17:02:34 -0500

    Feat (Banner): se agrega componente en contendor banner

--- src/containers/encontramos-tu-inmueble/Banner.jsx
+++ src/containers/encontramos-tu-inmueble/Banner.jsx
@@ -1,38 +1,49 @@
 'use client'
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay, Navigation } from "swiper";
+import { Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { stepTitles } from "@/helpers";
+import '@/sass/containers/encontramos-tu-inmueble/Banner.scss'
+import { Form } from "@/components/encontramos-tu-inmueble/Form";
 
 export const Banner = () => {
     return (
-        <Swiper
-            navigation={true}
-            modules={[Autoplay]}
-            slidesPerView={1}
-            spaceBetween={1}
-            loop={true}
-            autoplay={{ delay: 2000, disableOnInteraction: false }}
-            breakpoints={{
-                640: {
-                    slidesPerView: 1,
-                    spaceBetween: 2,
-                    navigation: false,
-                },
-                1024: {
-                    slidesPerView: 1,
-                    spaceBetween: 1,
-                    navigation: false,
-                },
-            }}
-        >
-            {stepTitles?.map((item) =>
-                <SwiperSlide key={item.id}>
-                    <h1 className=""><span>Paso{item.id}</span>{item.title}</h1>
-                </SwiperSlide>
-            )}
-        </Swiper>
+        <section className="container-banner">
+            <img className="img-figure" src="/encontramos-tu-inmueble/img/animated-figure.png" alt="" />
+            <div className="content-slider">
+                <Swiper
+                    modules={[Autoplay]}
+                    slidesPerView={1}
+                    spaceBetween={1}
+                    speed={1000}
+                    loop={true}
+                    autoplay={{ delay: 2000, disableOnInteraction: false }}
+                    breakpoints={{
+                        640: {
+                            slidesPerView: 1,
+                            spaceBetween: 2,
+                        },
+                        1024: {
+                            slidesPerView: 1,
+                            spaceBetween: 1,
+                        },
+                    }}
+                >
+                    {stepTitles?.map((item) =>
+                        <SwiperSlide key={item.id}>
+                            <h1 className="section-title"><span>Paso {item.id}</span>{item.title}</h1>
+                        </SwiperSlide>
+                    )}
+                </Swiper>
+                <img className="img-items" loading="lazy" src="/encontramos-tu-inmueble/img/items-icons.webp" alt="Image" />
+            </div>
+            <div className="content-form">
+                <h2>Dinos como buscas tu hogar y te ayudaremos a encontrarlo </h2>
+                <p>Completa el formualrio con tus preferencias</p>
+                <Form />
+            </div>
+        </section>
     )
 }
