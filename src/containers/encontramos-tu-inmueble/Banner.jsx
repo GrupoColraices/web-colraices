976334855a33efc2eff92b1b1b293c643dec4aff
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-26 17:00:15 -0500

    Feat (loading): se agrega mensaje enviado correctamente e imagenes requeridas

--- src/containers/encontramos-tu-inmueble/Banner.jsx
+++ src/containers/encontramos-tu-inmueble/Banner.jsx
@@ -7,43 +7,54 @@ import "swiper/css/pagination";
 import { stepTitles } from "@/helpers";
 import '@/sass/containers/encontramos-tu-inmueble/Banner.scss'
 import { Form } from "@/components/encontramos-tu-inmueble/Form";
+import { FaCheck } from "react-icons/fa6";
+import { useState } from "react";
 
-export const Banner = () => {
+export const Banner = ({ cities }) => {
+    const [loading, setLoading] = useState(false);
+
     return (
-        <section className="container-banner">
-            <img className="img-figure" src="/encontramos-tu-inmueble/img/animated-figure.png" alt="" />
-            <div className="content-slider">
-                <Swiper
-                    modules={[Autoplay]}
-                    slidesPerView={1}
-                    spaceBetween={1}
-                    speed={1000}
-                    loop={true}
-                    autoplay={{ delay: 2000, disableOnInteraction: false }}
-                    breakpoints={{
-                        640: {
-                            slidesPerView: 1,
-                            spaceBetween: 2,
-                        },
-                        1024: {
-                            slidesPerView: 1,
-                            spaceBetween: 1,
-                        },
-                    }}
-                >
-                    {stepTitles?.map((item) =>
-                        <SwiperSlide key={item.id}>
-                            <h1 className="section-title"><span>Paso {item.id}</span>{item.title}</h1>
-                        </SwiperSlide>
-                    )}
-                </Swiper>
-                <img className="img-items" loading="lazy" src="/encontramos-tu-inmueble/img/items-icons.webp" alt="Image" />
-            </div>
-            <div className="content-form">
-                <h2>Dinos como buscas tu hogar y te ayudaremos a encontrarlo </h2>
-                <p>Completa el formualrio con tus preferencias</p>
-                <Form />
-            </div>
-        </section>
+        <>
+            {loading && <p className="message-succes">Tu solicitud fue enviada con éxito <FaCheck color="#fff" />
+            </p>}
+            <section className="container-banner">
+                <img className="img-figure" loading="lazy" src="/encontramos-tu-inmueble/img/animated-figure.png" alt="figure background" />
+                <div className="content-slider">
+                    <Swiper
+                        modules={[Autoplay]}
+                        slidesPerView={1}
+                        spaceBetween={1}
+                        speed={1000}
+                        loop={true}
+                        autoplay={{ delay: 2000, disableOnInteraction: false }}
+                        breakpoints={{
+                            640: {
+                                slidesPerView: 1,
+                                spaceBetween: 2,
+                            },
+                            1024: {
+                                slidesPerView: 1,
+                                spaceBetween: 1,
+                            },
+                        }}
+                    >
+                        {stepTitles?.map((item) =>
+                            <SwiperSlide key={item.id}>
+                                <h1 className="section-title"><span>Paso {item.id}</span>{item.title}</h1>
+                            </SwiperSlide>
+                        )}
+                    </Swiper>
+                    <img className="img-items" loading="lazy" src="/encontramos-tu-inmueble/img/items-icons.webp" alt="Image" />
+                    <img className="check-bubble" loading="lazy" src="/encontramos-tu-inmueble/icons/check-bubble.svg" alt="figure background" />
+                    <img className="idea-bubble" loading="lazy" src="/encontramos-tu-inmueble/icons/idea-bubble.svg" alt="figure background" />
+                    <img className="notification-bubble" loading="lazy" src="/encontramos-tu-inmueble/icons/notification-bubble.svg" alt="figure background" />
+                </div>
+                <div className="content-form">
+                    <h2>Dinos como buscas tu hogar y te ayudaremos a encontrarlo </h2>
+                    <p>Completa el formualrio con tus preferencias</p>
+                    <Form cities={cities} setLoading={setLoading} />
+                </div>
+            </section >
+        </>
     )
 }
