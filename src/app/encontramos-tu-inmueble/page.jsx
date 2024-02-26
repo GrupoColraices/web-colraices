1dbc5af07e222f73638b83cfe760a316e74c961c
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-26 18:15:43 -0500

    Feat (metadata): se agrega metadatos de la pagina web

--- src/app/encontramos-tu-inmueble/page.jsx
+++ src/app/encontramos-tu-inmueble/page.jsx
@@ -1,6 +1,12 @@
 import { Banner } from '@/containers/encontramos-tu-inmueble/Banner'
 import { APIURL } from '@/helpers/api';
 
+
+export const metadata = {
+    title: ' Encontramos tu casa en Colombia | Venta de casas para colombianos en el exterior',
+    description: "Nos especializamos en encontrar casas y apartamento nuevos y usados en venta en ciudades como Bogotá, Cali y Barranquilla para colombianos en el exterior Además, ofrecemos asesoramiento en créditos hipotecarios y leasing habitacional.",
+    keywords: "Casa en colombia, casas para alquilar,compra de casas en Colombia,comprar casa Colombia,finca raiz cali,casas en venta barranquilla,casas en venta bogota,venta de casas en cali,invertir en vivienda,credito de vivienda usada,crédito para casa nueva,credito hipotecario en línea,davivienda leasing,leasing habitacional,credito bancario para casa,calcular credito hipotecario,bancolombia vivienda usada,Bancolombia vivienda nueva,compra de vivienda usada Davivienda"
+}
 export default async function WeFoundYourProperty() {
     const cities = await getCities();
     return (
