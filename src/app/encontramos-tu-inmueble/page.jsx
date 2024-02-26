e6827916dde358f94a099f068a0d70b9de58a222
Mmontoya1 <mauricio.montoya@colraices.co>
2024-02-26 16:17:10 -0500

    Feat (getCities): se agrega funcionalidad de consumo de API de ciudades

--- src/app/encontramos-tu-inmueble/page.jsx
+++ src/app/encontramos-tu-inmueble/page.jsx
@@ -1,9 +1,16 @@
 import { Banner } from '@/containers/encontramos-tu-inmueble/Banner'
+import { APIURL } from '@/helpers/api';
 
-export default function WeFoundYourProperty() {
+export default async function WeFoundYourProperty() {
+    const cities = await getCities();
     return (
         <>
-            <Banner />
+            <Banner cities={cities} />
         </>
     )
+}
+export async function getCities() {
+    const fetching = await fetch(`${APIURL}/cities`);
+    const response = await fetching.json();
+    return response?.data;
 }
