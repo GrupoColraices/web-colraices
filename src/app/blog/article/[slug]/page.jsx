87e3309dcb0e0e5e8c545e2cb2ae2b55bbfea287
Mmontoya1 <mauricio.montoya@colraices.co>
2024-03-13 18:25:19 -0500

    Feat (): Agrega página interna de artículos

--- src/app/blog/article/[slug]/page.jsx
+++ src/app/blog/article/[slug]/page.jsx
@@ -1,7 +1,34 @@
-import React from 'react'
+import { Header } from '@/components/Header'
+import SearchInputBlog from '@/components/blog/SearchInputBlog';
+import Top from '@/components/blog/Top';
+import '@/sass/containers/blog/ArticlePage.scss';
 
-export default function ArticlePage() {
+export default function ArticlePage(props) {
     return (
-        <div>page</div>
+        <>
+            <Header />
+            <section className='article-container'>
+                <SearchInputBlog />
+                <div className='article-content'>
+                    <article>
+                        <h1>Lorem</h1>
+                        <span>Lorem span</span>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.</p>
+                        <div className="image" />
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod
+                        </p>
+                    </article>
+                    <aside>
+                        <Top />
+                    </aside>
+
+                </div>
+            </section>
+        </>
+
     )
 }
