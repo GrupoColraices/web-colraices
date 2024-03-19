5b665936b29b839886dd4eb16d0fa34e3b247466
MauricioMontoya1 <mauricio.montoya@colraices.co>
2024-03-18 20:43:31 -0500

    Feat (Pagination): Agrega funcionalidad paginación de articulos

--- src/containers/blog/ArticlesSection.jsx
+++ src/containers/blog/ArticlesSection.jsx
@@ -1,6 +1,5 @@
 'use client'
-import React, { Suspense, useState } from 'react';
-import useSWR from 'swr';
+import React, { useContext, useState } from 'react';
 import Line from '@/components/Line';
 import CardArticleLarge from '@/components/blog/CardArticleLarge';
 import CardArticleMedium from '@/components/blog/CardArticleMedium';
@@ -8,15 +7,16 @@ import CardArticleSm from '@/components/blog/CardArticleSm';
 import '@/sass/containers/blog/ArticlesSection.scss';
 import '@/sass/components/blog/Pagination.scss'
 import ResponsivePagination from 'react-responsive-pagination'
-import { Loading } from '@/app/casas-apartamentos-colombia-desde-el-exterior/components/Loading';
+import { ArticlesContext } from '@/context/ArticlesContext';
 
 
-const ArticlesSection = ({ articles }) => {
+const ArticlesSection = () => {
+  const { filtered: articles } = useContext(ArticlesContext);
   const [currentPage, setCurrentPage] = useState(1);
   const [articleStart, setArticleStart] = useState(0)
   const [articleEnd, setArticleEnd] = useState(6)
 
-  const totalPages = articles.length / 6
+  const totalPages = articles?.length / 6 > 1 ? articles?.length / 6 : 1
   const handlePageChange = (page) => {
     if (page > currentPage) {
       setArticleStart(articleStart + (page - currentPage) * 6)
@@ -31,42 +31,37 @@ const ArticlesSection = ({ articles }) => {
   const sliceArticles = (start, end) => {
     return articles?.slice(start, end)
   }
-  console.log(sliceArticles(articleStart, articleEnd), "slice")
   return (
     <section>
       <h2>Artículos</h2>
       <Line width='20' />
-
-      <Suspense fallback={<p>Loading</p>}>
-
-
-        <div className='container-cards-article'>
-          {/* {articles?.slice(articleStart, articleEnd).map((article, index) =>
-            <CardArticleLarge key={index} article={article} />
-          )} */}
-          <div>
-            <CardArticleLarge article={sliceArticles(articleStart, articleEnd)[0]} />
+      {articles.length !== 0 ?
+        <>
+          <div className='container-cards-article'>
+            <div>
+              {(sliceArticles(articleStart, articleEnd)[0]) && <CardArticleLarge article={sliceArticles(articleStart, articleEnd)[0]} />}
+            </div>
+            {<div className='container-cards-medium'>
+              {(sliceArticles(articleStart, articleEnd)[1]) && <CardArticleMedium article={sliceArticles(articleStart, articleEnd)[1]} />}
+              {(sliceArticles(articleStart, articleEnd)[2]) && <CardArticleMedium article={sliceArticles(articleStart, articleEnd)[2]} />}
+            </div>}
+            <div className='container-cards-sm'>
+              {(sliceArticles(articleStart, articleEnd)[3]) && <CardArticleSm article={sliceArticles(articleStart, articleEnd)[3]} />}
+            </div>
+            <div className='container-cards-sm-desktop'>
+              {(sliceArticles(articleStart, articleEnd)[3]) && <CardArticleSm article={sliceArticles(articleStart, articleEnd)[3]} />}
+              {(sliceArticles(articleStart, articleEnd)[4]) && <CardArticleSm article={sliceArticles(articleStart, articleEnd)[4]} />}
+              {(sliceArticles(articleStart, articleEnd)[5]) && <CardArticleSm article={sliceArticles(articleStart, articleEnd)[5]} />}
+            </div>
           </div>
-          <div className='container-cards-medium'>
-            <CardArticleMedium article={sliceArticles(articleStart, articleEnd)[1]} />
-            <CardArticleMedium article={sliceArticles(articleStart, articleEnd)[2]} />
-          </div>
-
-          <div className='container-cards-sm-desktop'>
-            <CardArticleSm article={sliceArticles(articleStart, articleEnd)[2]} />
-            <CardArticleSm article={sliceArticles(articleStart, articleEnd)[4]} />
-            <CardArticleSm article={sliceArticles(articleStart, articleEnd)[5]} />
-          </div>
-        </div>
-        <div className="container-pagination">
-          <ResponsivePagination
-            total={totalPages}
-            current={currentPage}
-            onPageChange={page => handlePageChange(page)}
-          />
-        </div>
-      </Suspense>
-    </section>
+          <div className="container-pagination">
+            <ResponsivePagination
+              total={totalPages}
+              current={currentPage}
+              onPageChange={page => handlePageChange(page)}
+            />
+          </div>  </> : <p>No hay artículos que coincidan con tu búsqueda</p>}
+    </section >
 
   )
 }
