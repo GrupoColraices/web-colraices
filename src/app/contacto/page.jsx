e9cd704d316aa622a0746089b8f26b6b9fa8b6ea
MauricioMontoya1 <mauricio.montoya@colraices.co>
2024-03-23 08:26:12 -0500

    Feat (): Agrega página de contactos

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import { Header } from "@/components/Header"
+import { ItemFormResultado } from "@/components/ItemFormResultado"
+import '@/sass/containers/contacto/AssistedSearch.scss'
 
-export default function ContactPage() {
-    return <div>Contact</div>
+export const metadata = {
+    title: " Comprar casa en Colombia desde el exterior | Te contactamos"
+}
+export default function AssistedSearch() {
+    return (
+        <>
+            <Header />
+            <section className="assisted-search">
+                <ItemFormResultado />
+            </section>
+        </>
+
+    )
 }
