961aa905184e85083615b2abbeaacf70c1c71988
MauricioMontoya1 <mauricio.montoya@momentumdigital.com.co>
2024-03-23 08:50:38 -0500

    Refactor (nombre, className): Cambio nombre de componente y clase

--- src/app/contacto/page.jsx
+++ src/app/contacto/page.jsx
@@ -1,15 +1,15 @@
 import { Header } from "@/components/Header"
 import { ItemFormResultado } from "@/components/ItemFormResultado"
-import '@/sass/containers/contacto/AssistedSearch.scss'
+import '@/sass/containers/contacto/contacto.scss'
 
 export const metadata = {
     title: " Comprar casa en Colombia desde el exterior | Te contactamos"
 }
-export default function AssistedSearch() {
+export default function Contacto() {
     return (
         <>
             <Header />
-            <section className="assisted-search">
+            <section className="contacto">
                 <ItemFormResultado />
             </section>
         </>
