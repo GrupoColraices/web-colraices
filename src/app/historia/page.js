a47b359f6d6b0c00d1f5ae7ea9a7ebcd98a6cceb
MauricioMontoya1 <mauricio.montoya@momentumdigital.com.co>
2024-04-01 17:49:03 -0500

    Feat (): Agrega página historia

---
+++ src/app/historia/page.js
@@ -0,0 +1,12 @@
+import { Header } from '@/components/Header'
+import { Banner } from '@/containers/historia/Banner'
+import React from 'react'
+
+export default function History() {
+    return (
+        <>
+            <Header />
+            <Banner />
+        </>
+    )
+}
