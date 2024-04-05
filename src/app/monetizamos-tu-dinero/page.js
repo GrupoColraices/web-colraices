8ac07c622c3ef97a60b9531536cfa4f995fd9c45
cristianorregodev <cristian.duquew@gmail.com>
2024-04-05 09:46:46 -0500

    Feat (metadata): Agrega copys seo en meta tags

--- src/app/monetizamos-tu-dinero/page.js
+++ src/app/monetizamos-tu-dinero/page.js
@@ -5,7 +5,14 @@ import { MonetizationBanner } from '@/containers/monetizamos-tu-dinero/Monetizat
 import { BenefitsSection } from '@/containers/monetizamos-tu-dinero/BenefitsSection'
 import { Form } from '@/components/gestionamos-tu-credito/Form'
 
-const MonetizationPage = () => {
+export const metadata = {
+    title: 'Envía dinero a Colombia para tu casa propia l Colraices',
+    description:
+        'Con el servicio de monetización de Colraices, puedes enviar dinero a Colombia con confianza, aprovechando las mejores tasas del mercado. Facilitamos que los colombianos en el exterior envíen dinero para la compra de su vivienda en Colombia.',
+    keywords:
+        'Enviar dinero a Colombia, Monetización de remesas, Envío de dinero seguro a Colombia, Transferir dinero a Colombia desde el exterior, Servicio de envío de remesas a Colombia, Enviar dinero a familiares en Colombia, Remesas a Colombia',
+}
+export default function MonetizationPage() {
     return (
         <>
             <Header />
@@ -17,5 +24,3 @@ const MonetizationPage = () => {
         </>
     )
 }
-
-export default MonetizationPage
