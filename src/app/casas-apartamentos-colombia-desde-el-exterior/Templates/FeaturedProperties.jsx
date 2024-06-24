0dea640b231fcd1397d12e3597a9c6a8b728fb5e
MauricioMontoya1 <mauricio.montoya@momentumdigital.com.co>
2024-06-24 11:21:46 -0500

    Feat (h1): Modifica texto del titulo principal

--- src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/FeaturedProperties.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/FeaturedProperties.jsx
@@ -6,7 +6,7 @@ export default function FeaturedProperties({ inmFeatured }) {
     return (
         <section className='container-featured'>
             <div className='container-items-featured'>
-                <h1>Explora nuestras <span> propiedades destacadas</span></h1>
+                <h1>!Descubre el<span> Inmueble estrella!</span></h1>
                 <ul>
                     {itemsEligenos.map((item) => (
                         <li key={item.id}><img src={item.src} alt="Text icon" /><p>{item.text}</p></li>
