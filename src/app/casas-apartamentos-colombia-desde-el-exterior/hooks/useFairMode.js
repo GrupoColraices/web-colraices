37c77db9984e5260157ca253efce222f65b679c6
MauricioMontoya1 <mauricio.montoya@momentumdigital.com.co>
2024-07-04 15:52:18 -0500

    Feat () : Agrega nuevas funciones y variables que pertenecen al modo feria.

--- src/app/casas-apartamentos-colombia-desde-el-exterior/hooks/useFairMode.js
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/hooks/useFairMode.js
@@ -1,29 +1,70 @@
 "use client"
 
 import { usePathname } from "next/navigation";
+import { useState, useEffect } from 'react';
+import useSWR from 'swr';
 
 
-export const useFairMode = () => {
+export const useFairMode = (initialPrice, fairPrice) => {
     const pathName = usePathname();
-    const countries = [
-        "espana",
-        "suiza",
-        "alemania",
-        "reino-unido",
-        "canada",
-        "panama",
-        "mexico",
-        "costa-rica",
-        "peru",
-        "estados-unidos",
-        "emiratos-arabes-unidos"
-    ];
+    const [currency, setCurrency] = useState("COP");
+    const countries = {
+        // "españa": "EUR",
+        // "suiza": "CHF",
+        // "alemania": "EUR",
+        // "reino-unido": "GBP",
+        "canada": "CAD",
+        // "panama": "PAB",
+        // "mexico": "MXN",
+        // "costa-rica": "CRC",
+        // "peru": "PEN",
+        // "estados-unidos": "USD",
+        // "emiratos-arabes-unidos": "AED"
+    };
+    const foundCountry = Object.keys(countries).find(country => pathName.includes(country));
+
+    const { data: exchangeRate, error } = useSWR(
+        `https://api.exchangerate-api.com/v4/latest/COP`,
+        async (url) => {
+            const response = await fetch(url);
+            const data = await response.json();
+            return data.rates;
+        }
+    );
+    useEffect(() => {
+        if (foundCountry) {
+            setFairMode(true)
+            setCurrency(countries[foundCountry]);
+        }
+    }, [])
+
+    const converterCurrency = () => {
+        if (!exchangeRate || error) return 'Error obteniendo tasas de cambio';
+        const price = initialPrice * exchangeRate[currency]
+        const fairprice = fairPrice * exchangeRate[currency]
+        return { price, fairprice };
+    };
+
+    const handleCurrency = (newCurrency) => {
+        setCurrency(newCurrency);
+    };
 
     const fairRoutes = new Set(
-        countries.map(country => `/casas-apartamentos-colombia-desde-el-exterior/${country}`)
+        Object.keys(countries).map(country => `/casas-apartamentos-colombia-desde-el-exterior/${country}`)
     );
 
-    const fairMode = fairRoutes.has(pathName)
+    const [fairMode, setFairMode] = useState(fairRoutes.has(pathName));
 
-    return { fairMode }
+    const discountRate = fairPrice !== null && ((initialPrice - fairPrice) / initialPrice) * 100
+
+    return {
+        fairMode,
+        setFairMode,
+        fairRoutes,
+        countries,
+        convertedPrice: converterCurrency(),
+        currency,
+        setCurrency: handleCurrency,
+        discountRate,
+    }
 }
