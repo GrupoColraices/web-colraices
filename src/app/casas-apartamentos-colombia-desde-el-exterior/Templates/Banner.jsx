3fdaa44ab77bbbcf306d59d8be6f9f1b67fcc9c4
MauricioMontoya1 <mauricio.montoya@momentumdigital.com.co>
2024-07-11 16:26:47 -0500

    Feat (validación, imagen) : Agrega validación e imagen para el modo feria

--- src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/Banner.jsx
+++ src/app/casas-apartamentos-colombia-desde-el-exterior/Templates/Banner.jsx
@@ -14,13 +14,16 @@ export const Banner = (props) => {
     const observar = useRef(null)
     const [width, setWidth] = useState(false)
     const { fairMode, setFairMode, fairRoutes } = useContext(FairMode);
-
+    const [isFair, setIsFair] = useState(fairMode)
     useEffect(() => {
         if (window.innerWidth < 930) {
             setWidth(true)
         }
         if (fairRoutes.has(pathName)) {
             setFairMode(true);
+        }
+        if (pathName.includes("filtrados") && fairMode) {
+            setIsFair(false)
         }
         observar.current = new IntersectionObserver(function (entries) {
             setFid(entries[0].isIntersecting);
@@ -48,7 +51,7 @@ export const Banner = (props) => {
     return (
         <section>
             <div className={`sticky-barSearch banner ${!main && 'bannermin'}`} aria-disabled={fairMode} >
-                <div className='video' aria-disabled={fairMode}>
+                <div className={`video `} aria-disabled={isFair}>
                     <video autoPlay muted loop ref={video}>
                         <source src={"/portal-inmobiliario/video/banner.mp4"} type="video/mp4" />
                         <source src="/portal-inmobiliario/video/banner.avi" type='video/avi' />
@@ -57,7 +60,7 @@ export const Banner = (props) => {
                         <h1>La feria</h1>
                         <img src="/portal-inmobiliario/img/colraicesInmobiliario/home/persons.webp" alt="Image of family" />
                         <img src="/portal-inmobiliario/img/colraicesInmobiliario/home/red-stripe.webp" alt="Red stripe" />
-                        <img src="/portal-inmobiliario/img/colraicesInmobiliario/home/logo.svg" alt="Colraices logo" />
+                        <img src="/portal-inmobiliario/img/colraicesInmobiliario/home/logo-colraices.webp" alt="Colraices logo" />
                         <div className="content-text">
                             <h2>¡Aterrizó en <span>Canadá!</span></h2>
                             <p><span>18</span> <span> <strong>de julio</strong> al <strong>18</strong> de agosto </span> </p>
