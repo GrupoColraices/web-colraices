6fb3034b7c1fd7e4770dd1e8d502f1d9a2068839
MauricioMontoya1 <mauricio.montoya@momentumdigital.com.co>
2024-07-15 17:41:50 -0500

    Feat (console.log) : Agrega log de verificación

--- src/middleware.js
+++ src/middleware.js
@@ -17,7 +17,7 @@
  */
 
 import { NextResponse } from "next/server"
-import { headers } from 'next/headers'
+import requestIp from "request-ip"
 
 const GEOLOCATION_API = "https://ipinfo.io"
 const TOKEN = "0b05297d792e01"
@@ -35,9 +35,11 @@ const countryNames = {
     // "AE": "emiratos-árabes-unidos"
 };
 export async function middleware(request) {
-    const ip = (request.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
+    requestIp.mw();
+    // const ip = (request.headers.get('x-forwarded-for') ?? '127.0.0.1').split(',')[0]
     // let ip = request.headers.get('x-Forwarded-For')
-    console.log(ip)
+    const ip = request.clientIp;
+    console.log(ip, "IP")
 
     try {
         // const testIP = "184.71.130.183"
