77aad54928dcffc9bc78a6fb2f8314df960ac5fe
Mauricio Montoya Abadia <mmontoyaabadia@gmail.com>
2024-09-17 19:05:02 -0500

    Feat: Agrega componentes Header, Providers, y FormRequestAssistence a RootLayout; envueltos children con Providers

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,9 @@
 import '../sass/main.scss'
 import { Footer } from '../components/Footer'
 import { Montserrat } from 'next/font/google'
+import { Header } from '@/components/Header'
+import { Providers } from './providers'
+import { FormRequestAssistence } from '@/components/FormRequestAssistence'
 const montserrat = Montserrat({
     subsets: ['latin'],
     weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900']
@@ -8,10 +11,15 @@ const montserrat = Montserrat({
 
 export default function RootLayout({ children }) {
     return (
+
         <html lang="en" suppressHydrationWarning>
             <body className={montserrat.className}>
-                {children}
-                <Footer />
+                <Providers>
+                    <Header />
+                    {children}
+                    <FormRequestAssistence />
+                    <Footer />
+                </Providers>
             </body>
         </html>
     )
