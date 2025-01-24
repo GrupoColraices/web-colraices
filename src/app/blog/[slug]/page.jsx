24018fa6bdb252161325234d18f4b63cadb03256
Juan Navarrete <navarrete0725@gmail.com>
2025-01-24 11:59:58 -0500

    fix(deleteHeader): Eliminación componente Header en ArticlePage

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,10 +1,7 @@
-import { Header } from '@/components/Header'
-import TopArticles from '@/components/blog/TopArticles';
-import { formatDate } from '@/helpers/formatDate';
-import '@/sass/containers/blog/ArticlePage.scss';
-import Image from 'next/image';
-
-
+import TopArticles from '@/components/blog/TopArticles'
+import { formatDate } from '@/helpers/formatDate'
+import '@/sass/containers/blog/ArticlePage.scss'
+import Image from 'next/image'
 
 export async function generateMetadata({ params }) {
     const { titulo, description } = await getArticle(params.slug)
@@ -12,44 +9,37 @@ export async function generateMetadata({ params }) {
         title: titulo,
         description: description,
     }
-
 }
 
 const getArticle = async (slug) => {
     const article = await fetch(`https://blog.colraices.com/api/v1/posts/${slug}`)
     const response = await article.json()
-    return response?.data;
+    return response?.data
 }
 const getArticles = async () => {
-    const fetching = await fetch(`https://blog.colraices.com/api/v1/posts?category_id=8`);
-    const response = await fetching.json();
-    return response?.data;
+    const fetching = await fetch(`https://blog.colraices.com/api/v1/posts?category_id=8`)
+    const response = await fetching.json()
+    return response?.data
 }
 
-
 export default async function ArticlePage({ params }) {
-    const articles = await getArticles();
+    const articles = await getArticles()
     const article = await getArticle(params.slug)
 
     return (
-        <>
-            <Header />
-            <section className='article-container'>
-                <div className='article-content'>
-                    <article>
-                        <h1>{article.titulo}</h1>
-                        <span className='author'> {formatDate(article.creacion)}</span>
-                        <p>{article.description}</p>
-                        <Image width={705} height={500} className="image" src={article.imagen} alt={article.alt} />
-                        <div dangerouslySetInnerHTML={{ __html: article.contenido }}></div>
-                    </article>
-                    <aside>
-                        <TopArticles articles={articles} />
-                    </aside>
-
-                </div>
-            </section>
-        </>
-
+        <section className="article-container">
+            <div className="article-content">
+                <article>
+                    <h1>{article.titulo}</h1>
+                    <span className="author"> {formatDate(article.creacion)}</span>
+                    <p>{article.description}</p>
+                    <Image width={705} height={500} className="image" src={article.imagen} alt={article.alt} />
+                    <div dangerouslySetInnerHTML={{ __html: article.contenido }}></div>
+                </article>
+                <aside>
+                    <TopArticles articles={articles} />
+                </aside>
+            </div>
+        </section>
     )
 }
